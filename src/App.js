bcd210016b8f33ec214ce525a9293d9beac2b5d9
chaohuichen <chaohuichen@github.com>
2020-05-25 20:38:38 -0400

    switch using d3tree testing

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import './App.css';
 import Treegraph from './Treegraph';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import MyNavbar from './Navbar';
+import D3Tree from './D3Tree';
 function App() {
   return (
     <div>
       <MyNavbar />
-      <Treegraph />
+      {/* <Treegraph /> */}
+      <D3Tree />
     </div>
   );
 }
---
+++ src/D3Tree.js
@@ -0,0 +1,72 @@
+import React, { useState } from 'react';
+import Tree from 'react-d3-tree';
+const myTreeData = [
+  {
+    name: '1',
+
+    children: [
+      {
+        name: '2',
+      },
+      {
+        name: '3',
+      },
+    ],
+  },
+];
+const svgSquare = { shape: 'circle', shapeProps: { r: 20 } };
+function D3Tree() {
+  const [data, setData] = useState([
+    {
+      name: '1',
+
+      children: [
+        {
+          name: '2',
+        },
+        {
+          name: '3',
+        },
+      ],
+    },
+  ]);
+  let preorder = [8, 5, 1, 7];
+  const insert = () => {
+    let idx = 0;
+    let n = preorder.length;
+    function helper(lower, upper) {
+      if (idx === n) return;
+      let val = preorder[idx];
+      if (val < lower || val > upper) return;
+      let newNode = { name: '' + val, children: [] };
+      idx++;
+      let left = helper(lower, val);
+      if (left) newNode.children[0] = left;
+      let right = helper(val, upper);
+      if (right) newNode.children[1] = right;
+      // newNode.children[1] = helper(val, upper);
+      return newNode;
+    }
+    let a = helper(Number.MIN_SAFE_INTEGER, Number.MAX_SAFE_INTEGER);
+    console.log(a);
+    setData([a]);
+  };
+  return (
+    <div id='treeWrapper' style={{ width: '150em', height: '50em' }}>
+      <Tree
+        data={data}
+        orientation='vertical'
+        circleRadius={20}
+        textLayout={{
+          textAnchor: 'start',
+          x: -5,
+          y: 0,
+          transform: undefined,
+        }}
+      />
+      <button onClick={() => insert()}>Click</button>
+    </div>
+  );
+}
+
+export default D3Tree;
