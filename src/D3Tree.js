abcfb1f3cd3b1882c83bc8a1ab816578734966cc
chaohuichen <chaohuichen@github.com>
2020-05-26 18:05:35 -0400

    add the transition to the tree so the user can see the tree updated

--- src/D3Tree.js
+++ src/D3Tree.js
@@ -1,129 +1,111 @@
-import React, { useState } from "react";
-import Tree from "react-d3-tree";
-const myTreeData = [
-  {
-    name: "1",
+import React, { useState } from 'react';
+import Tree from 'react-d3-tree';
 
-    children: [
-      {
-        name: "2",
-      },
-      {
-        name: "3",
-      },
-    ],
-  },
-];
-const svgSquare = { shape: "circle", shapeProps: { r: 20 } };
-function D3Tree() {
-  const [data, setData] = useState([
-    {
-      name: "1",
-
-      children: [
-        {
-          name: "2",
-        },
-        {
-          name: "3",
-        },
-      ],
+const sleep = (milliseconds) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(resolve, milliseconds);
+  });
+};
+const svgCircle = { shape: 'circle', shapeProps: { r: 20 } };
+const style = {
+  links: { stroke: 'blue', strokeWidth: 2 },
+  nodes: {
+    node: {
+      circle: {},
+      name: {},
+      attributes: {},
     },
-  ]);
+    leafNode: {
+      circle: {},
+      name: {},
+      attributes: {},
+    },
+  },
+};
+function D3Tree() {
   const [heapData, setHeap] = useState([
     {
-      name: "1",
-      children: [
-        {
-          name: "2",
-        },
-        {
-          name: "3",
-        },
-      ],
+      name: '1',
+      children: [],
     },
   ]);
-  let dummyData = [1, 2, null, null, 5, null, 7, 8];
+  let dummyData = [1, 2, 3, 4, 5, 6, 7, 8];
   //function to build a tree
-  const buildTree = () => {
+  const buildTree = async (e) => {
+    e.preventDefault();
+
     const arr = dummyData.slice();
     let len = arr.length;
     let i = 0;
-    let left = 1 // i * 2 + 1;
-    let right = 2 // i * 2 + 2;
+    let left = 1; // i * 2 + 1;
+    let right = 2; // i * 2 + 2;
     let node;
     while (i < len) {
-      if (typeof arr[i] === "object" && arr[i] !== null) {
+      if (typeof arr[i] === 'object' && arr[i] !== null) {
         node = arr[i];
       } else {
         if (arr[i] === null) {
           i++;
           continue;
         }
-        node = { name: "" + arr[i], children: [] };
+        node = { name: '' + arr[i], children: [] };
         arr[i] = node;
       }
-      console.log(i, left, right, arr, node);
+
       if (left < len && arr[left] !== null && node !== null) {
-        node.children[0] = { name: "" + arr[left], children: [] };
+        node.children[0] = { name: '' + arr[left], children: [] };
         arr[left] = node.children[0];
+        setHeap({ ...arr[0] });
+        await sleep(2000);
       }
-      if(right < len && arr[right] !== null && node !== null) {
+      if (right < len && arr[right] !== null && node !== null) {
         if (!node.children[0]) {
-          node.children[0] = { name: "" + arr[right], children: [] };
+          node.children[0] = { name: '' + arr[right], children: [] };
           arr[right] = node.children[0];
+
+          setHeap({ ...arr[0] });
+          await sleep(2000);
         } else {
-          node.children[1] = { name: "" + arr[right], children: [] };
+          node.children[1] = { name: '' + arr[right], children: [] };
           arr[right] = node.children[1];
+          setHeap({ ...arr[0] });
+          await sleep(2000);
         }
       }
+
       i++;
-      left += 2
-      right += 2
+      left += 2;
+      right += 2;
     }
-    setHeap(arr[0]);
-    console.log("HEAP ", heapData, arr);
+    //setHeap(arr[0]);
+    console.log('HEAP ', heapData, 'arry', arr[0]);
   };
 
-  let preorder = [8, 5, 1, 7];
-  const insert = () => {
-    let idx = 0;
-    let n = preorder.length;
-    function helper(lower, upper) {
-      if (idx === n) return;
-      let val = preorder[idx];
-      if (val < lower || val > upper) return;
-      let newNode = { name: "" + val, children: [] };
-      idx++;
-      let left = helper(lower, val);
-      if (left) newNode.children[0] = left;
-      let right = helper(val, upper);
-      if (right) newNode.children[1] = right;
-      // newNode.children[1] = helper(val, upper);
-      return newNode;
-    }
-    let a = helper(Number.MIN_SAFE_INTEGER, Number.MAX_SAFE_INTEGER);
-    console.log(a);
-    setData([a]);
-  };
   return (
-    <section className="main-container">
-      <div id="treeWrapper" style={{ height: "50em", width: "100%" }}>
+    <section className='main-container'>
+      <div id='treeWrapper' style={{ height: '50em', width: '100%' }}>
         <Tree
           data={heapData}
-          translate={{ x: 100, y: 50 }}
-          orientation="vertical"
-          circleRadius={20}
+          translate={{ x: 900, y: 250 }}
+          orientation='vertical'
           textLayout={{
-            textAnchor: "start",
+            textAnchor: 'start',
             x: -5,
             y: 0,
             transform: undefined,
           }}
+          nodeSvgShape={svgCircle}
+          styles={style}
+          pathFunc='straight'
+          transitionDuration={1000}
         />
       </div>
       {/* <button style={{height: "fit-content"}} onClick={() => insert()}>Click</button> */}
-      <button key="bKey" style={{ height: "fit-content" }} onClick={() => buildTree()}>
+      <button
+        key='bKey'
+        style={{ height: 'fit-content' }}
+        onClick={(e) => buildTree(e)}
+      >
         Click
       </button>
     </section>
