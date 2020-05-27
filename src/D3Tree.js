d753aa0744e9fe0be07b97e89d09653502bd6060
chaohuichen <chaohuichen@github.com>
2020-05-27 17:18:00 -0400

    change the color when user travseral the tree

--- src/D3Tree.js
+++ src/D3Tree.js
@@ -6,7 +6,10 @@ const sleep = (milliseconds) => {
     setTimeout(resolve, milliseconds);
   });
 };
-const svgCircle = { shape: 'circle', shapeProps: { r: 20 } };
+const svgCircle = {
+  shape: 'circle',
+  shapeProps: { r: 20, stroke: 'red', fill: 'red' },
+};
 const style = {
   links: { stroke: 'blue', strokeWidth: 2 },
   nodes: {
@@ -22,6 +25,7 @@ const style = {
     },
   },
 };
+
 function D3Tree() {
   const [heapData, setHeap] = useState([
     {
@@ -31,9 +35,7 @@ function D3Tree() {
   ]);
   let dummyData = [1, 2, 3, 4, 5, 6, 7, 8];
   //function to build a tree
-  const buildTree = async (e) => {
-    e.preventDefault();
-
+  const buildTree = async () => {
     const arr = dummyData.slice();
     let len = arr.length;
     let i = 0;
@@ -56,7 +58,7 @@ function D3Tree() {
         node.children[0] = { name: '' + arr[left], children: [] };
         arr[left] = node.children[0];
         setHeap({ ...arr[0] });
-        await sleep(2000);
+        //await sleep(2000);
       }
       if (right < len && arr[right] !== null && node !== null) {
         if (!node.children[0]) {
@@ -64,12 +66,12 @@ function D3Tree() {
           arr[right] = node.children[0];
 
           setHeap({ ...arr[0] });
-          await sleep(2000);
+          //await sleep(2000);
         } else {
           node.children[1] = { name: '' + arr[right], children: [] };
           arr[right] = node.children[1];
           setHeap({ ...arr[0] });
-          await sleep(2000);
+          //await sleep(2000);
         }
       }
 
@@ -77,10 +79,39 @@ function D3Tree() {
       left += 2;
       right += 2;
     }
-    //setHeap(arr[0]);
+    // setHeap(arr]);
+
     console.log('HEAP ', heapData, 'arry', arr[0]);
   };
+  const singleStyle = {
+    shape: 'circle',
+    shapeProps: {
+      r: 20,
+      fill: 'blue',
+    },
+  };
+  const dfs = async (root) => {
+    // let node = Array.from(heapData)[0];
+    let node = root;
+    if (!node || node === 'undefined') return;
 
+    let stack = [node];
+    while (stack.length !== 0) {
+      if (node.children[0]) stack.push(node.children[0]);
+      node = stack.pop();
+      node.nodeSvgShape = singleStyle;
+      setHeap({ ...heapData });
+      await sleep(2000);
+      console.log(node.name);
+      if (node.children[1]) stack.push(node.children[1]);
+    }
+
+    // dfs(node.children[0]);
+    // node.nodeSvgShape = singleStyle;
+    // setHeap({ ... });
+    // await sleep(2000);
+    // dfs(node.children[1]);
+  };
   return (
     <section className='main-container'>
       <div id='treeWrapper' style={{ height: '50em', width: '100%' }}>
@@ -97,14 +128,21 @@ function D3Tree() {
           nodeSvgShape={svgCircle}
           styles={style}
           pathFunc='straight'
-          transitionDuration={1000}
+          transitionDuration={0}
         />
       </div>
       {/* <button style={{height: "fit-content"}} onClick={() => insert()}>Click</button> */}
       <button
         key='bKey'
         style={{ height: 'fit-content' }}
-        onClick={(e) => buildTree(e)}
+        onClick={() => buildTree()}
+      >
+        Click
+      </button>
+      <button
+        key='aKey'
+        style={{ height: 'fit-content' }}
+        onClick={() => dfs(heapData)}
       >
         Click
       </button>
